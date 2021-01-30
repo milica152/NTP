f39689c7b3f56d443ed7f5afe16c8829e422c221
unknown <milica.medic@gmail.com>
2021-01-30 02:49:30 +0100

    Registracija i login

--- FamilyTree/application/models.py
+++ FamilyTree/application/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dob = models.DateField
+    dod = models.DateField(null=True)
+    image = models.CharField(max_length=1000)
+    children = models.ManyToManyField("self")
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
--- FamilyTree/application/views.py
+++ FamilyTree/application/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import CreateUserForm
+from .models import Member
+
 
 # Create your views here.
+
+def index(request):
+    all_members = Member.objects.all()
+    return HttpResponse(render(request, 'application/index.html', {
+        'all_members': all_members,
+        'all_members_size': len(all_members),
+    }))
+
+
+class CreateUserFormView(View):
+    form_class = CreateUserForm
+    template_name = 'application/registration_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+            else:
+                return render(request, self.template_name, {'form': form})
+
+            return redirect('index')
+        else:
+            return render(request, self.template_name, {'form': form})
